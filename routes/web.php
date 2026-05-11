f9a79ffa542be29c8785fe8c1a04c7bface13589
fmjakees <fmjakee@gmail.com>
2026-05-11 11:43:52 +0700

    Pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -53,3 +56,13 @@ Route::get('/tugaspert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pengawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, "tentang"]);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+Route::get('/pegawai/', [PegawaiDBController::class, 'index']);
