e4c48bb1018bc2701887c2b80cec04aa8b4857b1
fmjakees <fmjakee@gmail.com>
2026-05-25 11:53:19 +0700

    Pertemuan 12

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; // Add this line
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive(); // Or useBootstrapFour() for older versions
     }
 }
